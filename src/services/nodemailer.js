77bae743cb94492a0a83372fbb5e2b30c70cc25b
Gustavoloppes-dev <gustavoloppes.dev@gmail.com>
2024-09-08 00:41:19 -0300

    fix:Corrigindo bug de importação

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -1,9 +1,15 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const nodemailer = require('nodemailer');
 
 const userGmail = process.env.USER_GMAIL;
 const senhaAppGmail = process.env.SENHA_APP_GMAIL;
 
 const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   service: "gmail",
   auth: {
     user: userGmail,
